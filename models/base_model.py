96c8d1233980b88dd6503dd976fe20a384e03bce
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-08-07 18:09:15 +0300

    Add save to storage

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
+            from models.__init__ import storage
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key in ("updated_at", "created_at"):
@@ -41,6 +43,8 @@ class BaseModel:
         Updates the updated_at value
         """
         self.updated_at = datetime.utcnow()
+        from models.__init__ import storage
+        storage.save()
 
     def to_dict(self):
         """
